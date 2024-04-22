1eb7abfb1df94fe8cb40588cca4ca81b991f936c
anuj7742 <anujkumawat7742@gmail.com>
2024-04-22 09:49:47 +0530

    First Commit

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Spinner.css"
+
+
+const Spinner = () => {
+  return (
+    <div className='flex justify-center mt-[100px]'>
+        <div className='spinner'></div>
+    </div>
+  )
+}
+
+export default Spinner
